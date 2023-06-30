7d43e7bdb10fc5ea9280b2296f86bff836738f53
blixondi <blixondi@gmail.com>
2023-06-30 16:23:29 +0700

    Update productlist.blade.php

--- resources/views/main/productlist.blade.php
+++ resources/views/main/productlist.blade.php
@@ -44,25 +44,7 @@
       <!--/.row -->
       <div class="grid grid-view projects-masonry shop mb-13">
         <div class="row gx-md-8 gy-10 gy-md-13 isotope">
-          {{-- <div class="project item col-md-6 col-xl-4">
-            <figure class="rounded mb-6">
-              <img src="{{asset("/assets/fe/img/photos/sh1.jpg")}}" srcset="{{asset("/assets/img/photos/sh1@2x.jpg 2x")}}" alt="" />
-              <a class="item-like" href="#" data-bs-toggle="white-tooltip" title="Add to wishlist"><i class="uil uil-heart"></i></a>
-              <a class="item-view" href="#" data-bs-toggle="white-tooltip" title="Quick view"><i class="uil uil-eye"></i></a>
-              <a href="#" class="item-cart"><i class="uil uil-shopping-bag"></i> Add to Cart</a>
-              <span class="avatar bg-pink text-white w-10 h-10 position-absolute text-uppercase fs-13" style="top: 1rem; left: 1rem;"><span>Sale!</span></span>
-            </figure>
-            <div class="post-header">
-              <div class="d-flex flex-row align-items-center justify-content-between mb-2">
-                <div class="post-category text-ash mb-0">Shoes</div>
-                <span class="ratings five"></span>
-              </div>
-              <h2 class="post-title h3 fs-22"><a href="./shop-product.html" class="link-dark">Nike Air Sneakers</a></h2>
-              <p class="price"><del><span class="amount">$55.00</span></del> <ins><span class="amount">$45.00</span></ins></p>
-            </div>
-            <!-- /.post-header -->
-          </div> --}}
-          <!-- /.item -->
+
           @foreach ($product as $p)
             <div class="project item col-md-6 col-xl-4">
                 <figure class="rounded mb-6">
@@ -85,30 +67,12 @@
                 <!-- /.post-header -->
             </div>
             <!-- /.item -->
-              
+
           @endforeach
         </div>
         <!-- /.row -->
       </div>
       <!-- /.grid -->
-      <nav class="d-flex justify-content-center" aria-label="pagination">
-        <ul class="pagination">
-          <li class="page-item disabled">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true"><i class="uil uil-arrow-left"></i></span>
-            </a>
-          </li>
-          <li class="page-item active"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true"><i class="uil uil-arrow-right"></i></span>
-            </a>
-          </li>
-        </ul>
-        <!-- /.pagination -->
-      </nav>
       <!-- /nav -->
     </div>
     <!-- /.container -->
